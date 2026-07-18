9b2e4053031a8bea21e745a4335639e1881354af
akanandhu <akananthu11@gmail.com>
2026-07-18 18:13:10 +0530

    refactor(ingestion): centralize error codes

--- features/ingestion/errors.ts
+++ features/ingestion/errors.ts
@@ -1,26 +1,20 @@
-export class IngestionError extends Error {
-  readonly code:
-    | "unsupported_file"
-    | "empty_file"
-    | "file_too_large"
-    | "encrypted_pdf"
-    | "scanned_pdf"
-    | "corrupt_file"
-    | "non_contract_text"
-    | "no_clauses";
+export const INGESTION_ERROR_CODES = [
+  "unsupported_file",
+  "empty_file",
+  "file_too_large",
+  "encrypted_pdf",
+  "scanned_pdf",
+  "corrupt_file",
+  "non_contract_text",
+  "no_clauses",
+] as const;
 
-  constructor(
-    message: string,
-    code:
-      | "unsupported_file"
-      | "empty_file"
-      | "file_too_large"
-      | "encrypted_pdf"
-      | "scanned_pdf"
-      | "corrupt_file"
-      | "non_contract_text"
-      | "no_clauses",
-  ) {
+export type IngestionErrorCode = (typeof INGESTION_ERROR_CODES)[number];
+
+export class IngestionError extends Error {
+  readonly code: IngestionErrorCode;
+
+  constructor(message: string, code: IngestionErrorCode) {
     super(message);
     this.name = "IngestionError";
     this.code = code;
